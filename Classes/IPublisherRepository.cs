15378b9e8069b9112a2befa3f40bfc6fe0dec417
Fishka1104 <stas11042016@gmail.com>
2025-05-26 18:19:32 +0300

    Delete Classes directory

--- Classes/IPublisherRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookApp1.Classes {
-    public interface IPublisherRepository {
-        void CreatePublisher(Publisher publisher);
-        Publisher? GetPublisherById(int id);
-        List<Publisher> GetPublishers();
-        void UpdatePublisher(Publisher publisher);
-        void DeletePublisher(int id); 
-    }
-}
