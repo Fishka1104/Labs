997b89a00eef15cc1547ebf4ab4755d7d4be7b65
Fishka1104 <stas11042016@gmail.com>
2025-05-26 18:17:22 +0300

    Delete Classes directory

--- Classes/IAuthorRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookApp1.Classes {
-    public interface IAuthorRepository {
-        void CreateAuthor(Author author);
-        Author? GetAuthorById(int id);
-        List<Author> GetAuthors();
-        void UpdateAuthor(Author author);
-        void DeleteAuthor(int id); 
-    }
-}
